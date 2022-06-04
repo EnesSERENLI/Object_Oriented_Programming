121b1ca0e7f1285247fdda4257d5f2ba2b94ab0f
EnesSERENLI <enes.serenli@hotmail.com>
2022-06-04 17:37:53 +0300

    Static Classes

---
+++ Static_Classes/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Classes
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            PersonalQuantity++; // The number of personnel will increase with each instance taken. Since it is defined as static, it will continue to increase continuously.
+            StartingDate = DateTime.Now;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public DateTime StartingDate { get; set; }
+
+        //Static Property
+        public static int PersonalQuantity { get; set; }
+
+        //Static List
+        public static List<Employee> employeeList = new List<Employee>();
+    }
+}
---
+++ Static_Classes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //The number will automatically increase with each personnel we add.
+
+            Employee employee1 = new Employee();
+            employee1.FirstName = "Enes";
+            employee1.LastName = "Serenli";
+
+            Console.WriteLine("Personal Quantity: "+Employee.PersonalQuantity);
+
+            Employee employee2 = new Employee();
+            employee2.FirstName = "Semih";
+            employee2.LastName = "Serenli";
+
+            Console.WriteLine("Personal Quantity: " + Employee.PersonalQuantity);
+
+            Employee employee3 = new Employee();
+            employee3.FirstName = "Fatih";
+            employee3.LastName = "Serenli";
+
+            Console.WriteLine("Personal Quantity: " + Employee.PersonalQuantity);
+
+            //Static values can be reached without taking instances.
+            Employee.employeeList.Add(employee1);
+            Employee.employeeList.Add(employee2);
+            Employee.employeeList.Add(employee3);
+
+
+            foreach (Employee employee in Employee.employeeList)
+            {
+                Console.WriteLine(employee.FirstName + " " + employee.LastName);
+            }
+            Console.Read();
+        }
+    }
+}
