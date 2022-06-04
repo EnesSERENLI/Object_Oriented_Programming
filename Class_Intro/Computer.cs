2a21b9c774ec90f4843f7466c049f49c4cd2db8f
EnesSERENLI <enes.serenli@hotmail.com>
2022-06-04 13:09:10 +0300

    Create Class - Constructor - Initializer

---
+++ Class_Intro/Computer.cs
@@ -0,0 +1,59 @@
+﻿using System.Windows.Forms;
+
+namespace Class_Intro
+{
+    public class Computer //Create a class
+    {
+        //Class Creation Rules
+        /*
+        1- Class names must be defined in PascalCase.
+        2- Class names cannot start with special characters and numbers.
+        */
+
+        /*
+         Access Modifiers => Erişim belirteçleri
+        -private
+        -internal
+        -public
+        -protected
+        -protected internal
+         */
+
+        //Field => Variables defined inside the class
+        internal string monitor;
+        internal string displayCard;
+
+        //Property
+        internal string Processor { get; set; }
+        public int RAM { get; set; }
+
+
+        public Computer() //Constructor 1
+        {
+            MessageBox.Show("No definition made");
+        }
+        public Computer(string monitor) //Constructor 2
+        {
+            this.monitor = monitor;
+        }
+
+        public Computer(string monitor,string displayCard) //Constructor 3
+        {
+            this.monitor=monitor;
+            this.displayCard=displayCard;
+        }
+        public Computer(string monitor, string displayCard, string processor) //Constructor 4
+        {
+            this.monitor = monitor;
+            this.displayCard = displayCard;
+            this.Processor = processor;
+        }
+        public Computer(string monitor, string displayCard, string processor, int ram) //Constructor 5
+        {
+            this.monitor = monitor;
+            this.displayCard = displayCard;
+            Processor = processor;
+            RAM = ram;
+        }
+    }
+}
---
+++ Class_Intro/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Class_Intro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Computer pc1 = new Computer();
+            pc1.monitor = "Asus";
+            pc1.displayCard = "Gforce";
+            pc1.Processor = "I7";
+            pc1.RAM = 8;
+
+            MessageBox.Show($"Mobitor: {pc1.monitor} Display Card: {pc1.displayCard} Processor: {pc1.Processor} RAM: {pc1.RAM}");
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            Computer pc2 = new Computer("LG");
+            pc2.RAM = 4;
+            pc2.Processor = "I5";
+            pc2.displayCard = "NVdia";
+
+            MessageBox.Show($"Mobitor: {pc2.monitor} Display Card: {pc2.displayCard} Processor: {pc2.Processor} RAM: {pc2.RAM}");
+
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            Computer pc3 = new Computer("ViewSonic", "RTX 3080", "I9", 16);
+            MessageBox.Show($"Mobitor: {pc3.monitor} Display Card: {pc3.displayCard} Processor: {pc3.Processor} RAM: {pc3.RAM}");
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            //Initializer => We can create the object while getting the instance without needing the constructor.
+            Computer pc4 = new Computer
+            {
+                monitor = "Rampage",
+                displayCard = "RTX2060",
+                Processor = "I7",
+                RAM = 32
+            };
+            MessageBox.Show($"Mobitor: {pc4.monitor} Display Card: {pc4.displayCard} Processor: {pc4.Processor} RAM: {pc4.RAM}");
+        }
+    }
+}
